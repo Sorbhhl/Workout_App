1a247f603a2eb6cafde0075884240adabbaf9d28
Sorbhhl <sorbhhl@gmail.com>
2023-06-18 00:51:24 +0100

    180623_02

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const displayWorkout = document.querySelector('.current-workout')
 const countDownContainer = document.querySelector('.cont-countdown-circle')
 const countDownSpan = document.querySelector('.countdownCont span')
 const barValFullWk = document.querySelector('.bar-value-full-wk')
+const getReadyContainer = document.querySelector('.get-ready-cont')
 
 //Time display
 const timeDisplay = document.querySelector('.time-display')
@@ -17,7 +18,6 @@ let workoutRepetitions = 2;
 const progressWkValue = document.querySelector('.progressbar_value');
 const progressWk = document.querySelector('progress');
 let progressBarWk = 0;
-let initialBarTime = 0;
 
 //Exercises
 let currentWorkoutCont = document.querySelector('.current-workout-img');
@@ -33,15 +33,15 @@ const exercises =["./resources/gifs/a93c82108677535.5fc3684e78f67.gif",
 timeSelect.forEach(option =>{
     option.addEventListener('click', function() {
         defaultDuration = this.getAttribute('data-time');
-        if (defaultDuration == 300){
+        if (defaultDuration == 370){ //300 seconds (5 minutes) + 7 seconds of rest each workout (70 seconds)
             workoutRepetitions = 2
-            barValFullWk.textContent = '5 minutes'
-        }else if(defaultDuration == 600){
+            barValFullWk.textContent = '6 minutes'
+        }else if(defaultDuration == 740){ //600 seconds (10 minutes) + 7 seconds of rest each workout (140 seconds)
             workoutRepetitions = 4
-            barValFullWk.textContent = '10 minutes'
-        }else{
+            barValFullWk.textContent = '12 minutes'
+        }else{ //900 seconds (15 minutes) + 7 seconds of rest each workout (210 seconds)
             workoutRepetitions = 6
-            barValFullWk.textContent = '15 minutes'
+            barValFullWk.textContent = '18 minutes'
         }    
     })
 })
@@ -67,10 +67,9 @@ const workoutOnStart = (song, play, displayWorkout, workoutRepetitions) => {
     play.textContent = "◼"
     displayWorkout.style.height = "75%";
     changeCurrentWorkout(workoutRepetitions);
-    //Progress bar start
-    setInterval(function() {
-        updateProgressValue();
-    }, 1000);
+    timeSelect[0].disabled = true;
+    timeSelect[1].disabled = true;
+    timeSelect[2].disabled = true;
 }
 
 const workoutOnStop = (song, play, displayWorkout, workoutRepetitions) => {
@@ -79,12 +78,14 @@ const workoutOnStop = (song, play, displayWorkout, workoutRepetitions) => {
     play.classList.remove("play-btn-on-pause");
     play.textContent = "▶"
     displayWorkout.style.height = "0%";
+    timeSelect[0].disabled = false;
+    timeSelect[1].disabled = false;
+    timeSelect[2].disabled = false;
     //Restart workout
     countDownSpan.textContent = '';
     currentWorkout.src = exercises[0];
     workoutRepetitions = 0;
-    //Progress bar stop
-    initialBarTime = 0;
+    song.currentTime = 0;
 }
 
 //Progress bar for the full workout routine
@@ -93,15 +94,11 @@ const setBarValue = (value) => {
     progressWk.value = value;
 }
 
-const progressupdate = () => {
-    //Animate the progress bar for the full workout routine
-    progressBarWk = initialBarTime*100/defaultDuration;
+//Song and progress workout bar
+song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    progressBarWk = currentTime*100/defaultDuration;
     setBarValue(progressBarWk);
-}
-
-const updateProgressValue = () => {
-    if (initialBarTime == 100) clearInterval(this);
-    else initialBarTime++;
 }
 
 //Change workout exercise every 30 seconds and 7 seconds of rest
@@ -112,34 +109,37 @@ const sleep = async (milliseconds) => {
 };
 
 const changeCurrentWorkout = async (workoutRepetitions) =>{
-    for (let i=0 ; i<workoutRepetitions; i++){
+    for (let n=0 ; n<workoutRepetitions; n++){
         for (let i=0 ; i<5 ; i++){
             //3, 2, 1 Go!
-            currentWorkoutCont.style.display = 'none';
-            countDownContainer.style.display = 'block';
+            currentWorkout.src = exercises[i];
+            currentWorkout.style.height = '200px';
+            getReadyContainer.style.display = 'block';
             timer();
             await sleep(7000);
-            //Start the workout!     
+            //Start the workout!   
+            currentWorkout.style.height = '100%';  
             let workoutCounter = 30;
             workoutTimer(workoutCounter); 
-            currentWorkout.src = exercises[i];
-            currentWorkoutCont.style.display = 'block';
-            countDownContainer.style.display = 'none';
+            getReadyContainer.style.display = 'none';
             await sleep(5000);
             if(play.textContent == "▶"){
                 return
             }
         }
+        console.log(`Workout round: ${n+1}`)
     }
+    currentWorkout.src = './resources/gifs/congrats.gif';
+    await sleep(3000);
+    play.textContent = "Restart workout"
 }
 
 //Exercises counter
 const workoutTimer = (workoutCounter) => {
     let workoutTimer = setInterval( () => {
-        timeDisplay.textContent = `0:${workoutCounter}`
+        timeDisplay.textContent = (workoutCounter<10?`0:0${workoutCounter}`:`0:${workoutCounter}`)
         workoutCounter--;
-        console.log(workoutCounter)
-        if (workoutCounter == -1) clearInterval(workoutTimer);
+        if (workoutCounter == -1 || play.textContent == "▶") clearInterval(workoutTimer);
     }, 1000)
 }
 
